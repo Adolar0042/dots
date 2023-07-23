8cd6bfd4b5b71af59454b44fb0e5ece78fb457d5
Adolar0042 <39769465+Adolar0042@users.noreply.github.com>
2023-07-23 22:05:56 +0200

    Update dots.ps1

--- dots.ps1
+++ dots.ps1
@@ -14,6 +14,8 @@ if ( -NOT $thread_security_principal.IsInRole("Administrators") ) {
     exit 2
 }
 
+$rawRepoUrl = "https://raw.githubusercontent.com/Adolar0042/dots/main"
+
 Write-Host "Installing Cascadia Code Nerd Font ..."
 Invoke-WebRequest "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/CascadiaCode.zip" -OutFile "$($env:TEMP)\CascadiaCode.zip"
 Expand-Archive -Path "$($env:TEMP)\CascadiaCode.zip" -DestinationPath "$($env:TEMP)\CascadiaCode" -Force
@@ -21,12 +23,12 @@ foreach ($file in Get-ChildItem -Path "$($env:TEMP)\CascadiaCode" -Filter "*.ttf
     Copy-Item -Path $file.FullName -Destination "$($env:windir)\Fonts" -Force
 }
 Write-Host "Configuring Windows Terminal..."
-Invoke-RestMethod -Uri "https://raw.githubusercontent.com/Adolar0042/dots/main/settings.json" -OutFile "$($env:LOCALAPPDATA)\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
+Invoke-RestMethod -Uri "$rawRepoUrl/settings.json" -OutFile "$($env:LOCALAPPDATA)\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
 
 Write-Host "Installing Powershell Profile ..."
-Invoke-RestMethod -Uri "https://raw.githubusercontent.com/Adolar0042/dots/main/user_profile.ps1" -OutFile "$($env:USERPROFILE)\.config\PowerShell\user_profile.ps1"
+Invoke-RestMethod -Uri "$rawRepoUrl/user_profile.ps1" -OutFile "$($env:USERPROFILE)\.config\PowerShell\user_profile.ps1"
 New-Item -Path $PROFILE -Value '. "$env:USERPROFILE\.config\PowerShell\user_profile.ps1"' 
-Invoke-RestMethod -Uri "https://raw.githubusercontent.com/Adolar0042/dots/main/subscripts/Windows-Repair-Tool.ps1" -OutFile "$($env:USERPROFILE)\.config\PowerShell\subscripts\Windows-Repair-Tool.ps1"
+Invoke-RestMethod -Uri "$rawRepoUrl/subscripts/Windows-Repair-Tool.ps1" -OutFile "$($env:USERPROFILE)\.config\PowerShell\subscripts\Windows-Repair-Tool.ps1"
 
 Write-Host "Installing Chocolatey ..."
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
@@ -37,7 +39,7 @@ choco install sudo -y
 Write-Host "Installing Starship ..."
 winget install --id Starship.Starship
 Write-Host "Configuring Starship ..."
-Invoke-RestMethod -Uri "https://raw.githubusercontent.com/Adolar0042/dots/main/starship.toml" -OutFile "$($env:USERPROFILE)\.config\starship.toml"
+Invoke-RestMethod -Uri "$rawRepoUrl/starship.toml" -OutFile "$($env:USERPROFILE)\.config\starship.toml"
 Write-Host "Note: You need to restart your terminal to see the changes."
 
 Write-Host "Installing Git ..."
