4caca109e183c954028624f7cbeb802015e40e25
Adolar0042 <39769465+Adolar0042@users.noreply.github.com>
2023-07-24 00:19:30 +0200

    Update dots.ps1

--- dots.ps1
+++ dots.ps1
@@ -8,10 +8,10 @@
 $thread_security_principal = `
   [Security.Principal.WindowsPrincipal]([Threading.Thread]::CurrentPrincipal)
 if ( -NOT $thread_security_principal.IsInRole("Administrators") ) {
-    Write-Host "Restarting in elevated mode..." -ForegroundColor Yellow
-    $argv = @($MyInvocation.MyCommand.Definition) + $args
-    start-process "powershell.exe" -Arg $argv -Verb RunAs
-    exit 2
+  Write-Host "Restarting in elevated mode..." -ForegroundColor Yellow
+  $argv = @($MyInvocation.MyCommand.Definition) + $args
+  start-process "powershell.exe" -Arg $argv -Verb RunAs
+  exit 2
 }
 
 $rawRepoUrl = "https://raw.githubusercontent.com/Adolar0042/dots/main"
@@ -20,15 +20,27 @@ Write-Host "Installing Cascadia Code Nerd Font ..."
 Invoke-WebRequest "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/CascadiaCode.zip" -OutFile "$($env:TEMP)\CascadiaCode.zip"
 Expand-Archive -Path "$($env:TEMP)\CascadiaCode.zip" -DestinationPath "$($env:TEMP)\CascadiaCode" -Force
 foreach ($file in Get-ChildItem -Path "$($env:TEMP)\CascadiaCode" -Filter "*.ttf") {
-    Copy-Item -Path $file.FullName -Destination "$($env:windir)\Fonts" -Force
+  Copy-Item -Path $file.FullName -Destination "$($env:windir)\Fonts" -Force
 }
 Write-Host "Configuring Windows Terminal..."
 Invoke-RestMethod -Uri "$rawRepoUrl/settings.json" -OutFile "$($env:LOCALAPPDATA)\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
 
 Write-Host "Installing Powershell Profile ..."
+if (!(Test-Path "$($env:USERPROFILE)\.config\PowerShell")) {
+  New-Item -Path "$($env:USERPROFILE)\.config\PowerShell" -ItemType Directory
+}
 Invoke-RestMethod -Uri "$rawRepoUrl/user_profile.ps1" -OutFile "$($env:USERPROFILE)\.config\PowerShell\user_profile.ps1"
-New-Item -Path $PROFILE -Value '. "$env:USERPROFILE\.config\PowerShell\user_profile.ps1"' 
+if (!(Test-Path $PROFILE)) {
+  New-Item -Path $PROFILE -Value '. "$env:USERPROFILE\.config\PowerShell\user_profile.ps1"' 
+}
+else {
+  Add-Content -Path $PROFILE -Value '. "$env:USERPROFILE\.config\PowerShell\user_profile.ps1"'
+}
 Invoke-RestMethod -Uri "$rawRepoUrl/subscripts/Windows-Repair-Tool.ps1" -OutFile "$($env:USERPROFILE)\.config\PowerShell\subscripts\Windows-Repair-Tool.ps1"
+
+Write-Host "Installing various modules - user input may be needed ..."
+Install-Module -Name PSReadLine -Force
+Install-Module -Name posh-git -Force
 
 Write-Host "Installing Chocolatey ..."
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
